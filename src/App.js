f4ebfaa4bd7695a7317bac5daa42d1cd2a7434ac
NguyenTanTienQE180034 <mopmetyl@gmail.com>
2024-09-17 07:42:07 +0700

    tien

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-
 import './App.css';
 
 function App() {
   return (
-   <div>
-    <p>
-      Hello world
-    </p>
-   </div>
+    <div className="form-container">
+      <h2>Student Information Form</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" placeholder="Enter your name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="mssv">MSSV:</label>
+          <input type="text" id="mssv" placeholder="Enter your MSSV" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="phone">Phone Number:</label>
+          <input type="text" id="phone" placeholder="Enter your phone number" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input type="date" id="dob" />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }
 
